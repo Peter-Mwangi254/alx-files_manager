1283b0416426ce19e86031650fda41aba8df4fbc
Peter-Mwangi254 <pm6093628@gmail.com>
2024-08-02 02:57:03 -0700

    created an express server with relevant api endpoints

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/api', routes);
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
