b98854f9370f7a5bb36f1ed3fca237e8c8d7c96d
Peter-Mwangi254 <pm6093628@gmail.com>
2024-08-07 03:46:26 -0700

    fixed eslint error

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,3 +1,4 @@
+const mime = require('mime-types');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
@@ -172,6 +173,7 @@ class FilesController {
       parentId: file.parentId.toString(),
     });
   }
+
   static async getFile(req, res) {
     const fileId = req.params.id;
     const token = req.headers['x-token'];
@@ -204,7 +206,7 @@ class FilesController {
       const mimeType = mime.lookup(file.name) || 'application/octet-stream';
 
       res.setHeader('Content-Type', mimeType);
-      res.send(fileContent);
+      return res.send(fileContent);
     } catch (err) {
       return res.status(404).json({ error: 'Not found' });
     }
