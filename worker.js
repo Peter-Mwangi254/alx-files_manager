f1096ccdb10bf73a78bacf90442086a3ef60bd74
Peter-Mwangi254 <pm6093628@gmail.com>
2024-08-07 10:17:26 -0700

    Image Thumbnails

--- worker.js
+++ worker.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+const Bull = require('bull');
+const imageThumbnail = require('image-thumbnail');
+const fs = require('fs');
+const path = require('path');
+const dbClient = require('../utils/db');
+
+const fileQueue = new Bull('fileQueue');
+
+fileQueue.process(async (job) => {
+  const { userId, fileId } = job.data;
+
+  if (!fileId) throw new Error('Missing fileId');
+  if (!userId) throw new Error('Missing userId');
+
+  const file = await dbClient.findFileById(fileId);
+  if (!file || file.userId.toString() !== userId) {
+    throw new Error('File not found');
+  }
+
+  const widths = [500, 250, 100];
+  const options = { responseType: 'base64' };
+
+  for (const width of widths) {
+    const thumbnail = await imageThumbnail(file.localPath, { ...options, width });
+    const thumbnailPath = `${file.localPath}_${width}`;
+    await fs.promises.writeFile(thumbnailPath, Buffer.from(thumbnail, 'base64'));
+  }
+});
